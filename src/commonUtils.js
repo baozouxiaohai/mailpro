07e7d30c0e3f67df8a329594a053f4f97ca8abea
baozouxiaohai <baozouxiaohai@gmail.com>
2026-05-03 22:19:45 +0800

    Add files via upload

--- src/commonUtils.js
+++ src/commonUtils.js
@@ -1,5 +1,5 @@
 export function generateRandomId(length = 8) {
-  const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const chars = 'abcdefghjkmnpqrstuvwxyz23456789._-';
   let result = '';
   const len = Math.max(4, Math.min(32, Number(length) || 8));
   for (let i = 0; i < len; i++) {
